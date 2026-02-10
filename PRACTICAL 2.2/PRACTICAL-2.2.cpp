0933c23226c453d1d9f9dbca727d724e7786d7f5
vishv-vaghasiya <vishvaghasiya02@gmail.com>
2026-02-10 10:52:28 +0530

    Add files via upload

---
+++ PRACTICAL 2.2/PRACTICAL-2.2.cpp
@@ -0,0 +1,77 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+class student
+{
+    int roll_no;
+    string name;
+    int marks_1;
+    int marks_2;
+    int average;
+
+public:
+    student()
+    {
+        roll_no=1;
+        name="xyz";
+        marks_1=0;
+        marks_2=0;
+        calculateAverage();
+    }
+
+    student(int r,string n,int m1,int m2)
+    {
+        roll_no=r;
+        name=n;
+        marks_1=m1;
+        marks_2=m2;
+        calculateAverage();
+    }
+
+    void calculateAverage()
+    {
+        average = (marks_1 + marks_2) / 2;
+    }
+
+    void display()
+    {
+        cout << "\nRoll No: " << roll_no;
+        cout << "\nName: " << name;
+        cout << "\nMarks: " << marks_1 << ", "<< marks_2;
+        cout << "\nAverage Marks: " << average << endl;
+    }
+};
+
+int main()
+{
+    cout<<"NAME :-VISHV VAGHASIYA\n";
+    cout<<"ID NO:-25CE130\n\n";
+
+    int r,m1,m2,k;
+    string n;
+    cout<<"Enter the number of student: ";
+    cin>>k;
+    student s[k];
+
+    for(int i=0; i<k; i++)
+    {
+        cout<<"Enter the name: ";
+        cin>>n;
+        cout<<"Enter the roll_no: ";
+        cin>>r;
+        cout<<"Enter the marks of 1 subject: ";
+        cin>>m1;
+        cout<<"Enter the marks of 2 subject: ";
+        cin>>m2;
+        s[i] = student(r, n, m1, m2);
+    }
+
+    cout << "\n--- Student Records ---";
+    for (int i=0; i < k; i++)
+    {
+        s[i].display();
+    }
+
+    return 0;
+}
